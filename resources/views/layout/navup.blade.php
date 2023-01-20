7457bfc6ec30faa0fcc44f86450fce449c3cb530
Romina Trazzi <romina.trazzi@posta.it>
2023-01-20 04:19:44 +0100

    Commit cambiamenti grafici navbar pt 2

--- resources/views/layout/navup.blade.php
+++ resources/views/layout/navup.blade.php
@@ -1,17 +1,23 @@
 <!-- Superior Navbar -->
 <nav>
-     <div class="">
-            
-        </div>
+    
+    <div></div>
     
     <div class="nav_box">  
 
-        <ul class="menu_list d_flex">
-            <li> <a href="/home.php"> HOME </a></li>
-            <li> <a href="/aboutCeline.php"> ABOUT </a></li>
-            <li> <a href="/bookstore.php"> BOOKSTORE </a></li>
-            <li> <a href="#"> BLOG </a></li>
-            <li> <a href="/contacts.php"> CONTACTS </a></li>
+        <ul class="menu_list blended_bg ">
+            
+          <div class="background_layer d_flex">
+
+                <li> <a href="/home.php"> HOME </a></li> 
+                <li> <a href="/aboutCeline.php"> ABOUT </a></li>
+                <li> <a href="/bookstore.php"> BOOKSTORE </a></li>
+                <li> <a href="#"> BLOG </a></li>
+                <li> <a href="/contacts.php"> CONTACTS </a></li>
+
+            </div> 
+        
+        
         </ul>
     
     </div>
