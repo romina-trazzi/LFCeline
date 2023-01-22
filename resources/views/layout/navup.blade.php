c65313893d750e74b3df5c2a053da46d0c8424c8
Romina Trazzi <romina.trazzi@posta.it>
2023-01-22 20:57:02 +0100

    Commit navbar mobile

--- resources/views/layout/navup.blade.php
+++ resources/views/layout/navup.blade.php
@@ -1,13 +1,16 @@
 <!-- Superior Navbar -->
-<nav>
+<nav class="my_nav">
     
     <div></div>
     
-    <div class="nav_box">  
-
-        <ul class="menu_list blended_bg ">
+    <div class="nav_box"> 
+        
+        <!-- Trick: Check_box + Label -->
+        <input class="menu_btn" id="menu_btn" type="checkbox">
+        
+        <ul class="menu_list blended_bg">
             
-            <div class="background_layer d_flex">
+            <div class="background_layer"> 
 
                 <li> <a href="/home.php"> HOME </a></li> 
                 <li> <a href="/aboutCeline.php"> BIOGRAFIA </a></li>
@@ -19,7 +22,12 @@
         
         
         </ul>
-    
+
+        <label for="menu_btn" class="menu_icon">
+            <span class="nav_icon"></span>
+        </label>
+        
+        
     </div>
 </nav>
 
